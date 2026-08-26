97f4ac38a677df4fe1405efd0fae806273ce95fa
joaomanhaes1 <joao.manhaes.silva@escola.pr.gov.br>
2026-08-25 21:40:20 -0300

    dsa

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botao = document.getElementById("botao-tema");
 // Verifica se já tinha um tema salvo
 if (localStorage.getItem("tema") === "escuro") {
   document.body.classList.add("escuro");
-  botao.textContent = "Claro";
+  botao.textContent = "Modo Claro";
 }
 
 botao.addEventListener("click", () => {
@@ -11,9 +11,9 @@ botao.addEventListener("click", () => {
 
   if (document.body.classList.contains("escuro")) {
     localStorage.setItem("tema", "escuro");
-    botao.textContent = "Claro";
+    botao.textContent = "Modo Claro";
   } else {
     localStorage.setItem("tema", "claro");
-    botao.textContent = "Escuro";
+    botao.textContent = "Modo Escuro";
   }
 });
