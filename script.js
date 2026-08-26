fbe29968a72f10aa52b500a7894bc5a722c01b6b
joaomanhaes1 <joao.manhaes.silva@escola.pr.gov.br>
2026-08-26 06:18:12 -0300

    okkk

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const botao = document.getElementById("botao-tema");
 
-
-if (localStorage.getItem("tema") === "escuro") {
-  document.body.classList.add("escuro");
-  botao.textContent = "Modo Claro";
-}
-
-botao.addEventListener("click", () => {
-  document.body.classList.toggle("escuro");
-
-  if (document.body.classList.contains("escuro")) {
-    localStorage.setItem("tema", "escuro");
-    botao.textContent = "Modo Claro";
-  } else {
-    localStorage.setItem("tema", "claro");
-    botao.textContent = "Modo Escuro";
-  }
-});
 
 const botao = document.querySelector('#botao-tema');
 
